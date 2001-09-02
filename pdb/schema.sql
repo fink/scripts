a540539cdcb4b60f33a7f8e99ca0510c819063c2
Christoph Pfisterer <chrisp@users.sourceforge.net>
2001-09-02 10:46:49 +0000

    Imported package database scripts, step one.

---
+++ pdb/schema.sql
@@ -0,0 +1,22 @@
+create table release (
+ name varchar(20) not null,
+ description text,
+ primary key (name)
+);
+
+create table package (
+ release varchar(20) not null,
+ fullname varchar(128) not null,
+ name varchar(64) not null,
+ version varchar(64) not null,
+ revision varchar (16) not null,
+ section varchar(32) not null,
+ descshort varchar(80) not null default '',
+ desclong text,
+ maintainer varchar(255),
+ homepage varchar(255),
+ latest tinyint not null default '0',
+ needtest tinyint not null default '0',
+ primary key (release,name),
+ index flag (flag)
+);
