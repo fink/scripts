a360cef2a27aa9fdf0a96c852844ffa4cee49bed
Christoph Pfisterer <chrisp@users.sourceforge.net>
2001-09-04 13:05:21 +0000

    Updated for 0.2.6.

--- pdb/releases.sql
+++ pdb/releases.sql
@@ -1,4 +1,4 @@
 delete from release;
 insert into release values ('current-stable', 'Packages that are in stable in CVS, updated daily.');
 insert into release values ('current-unstable', 'Packages that are in unstable in CVS, updated daily.');
-insert into release values ('0.2.5-stable', 'Packages shipped with Fink 0.2.5');
+insert into release values ('0.2.6-stable', 'Packages shipped with Fink 0.2.6');
