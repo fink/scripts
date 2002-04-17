ae556e74488c08008fbc3b2ed2a490a101996f90
Max Horn <fingolfin@users.sourceforge.net>
2002-04-17 23:43:29 +0000

    0.4.0

--- pdb/releases.sql
+++ pdb/releases.sql
@@ -1,4 +1,4 @@
 delete from release;
 insert into release values ('current-stable', 'Packages that are in stable in CVS, updated daily.');
 insert into release values ('current-unstable', 'Packages that are in unstable in CVS, updated daily.');
-insert into release values ('0.3.2-stable', 'Packages shipped with Fink 0.3.2');
+insert into release values ('0.4.0-stable', 'Packages shipped with Fink 0.4.0');
