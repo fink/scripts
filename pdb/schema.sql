e8d7b393feca381925569c4a734eded785bcf257
Ben Hines <benh57@users.sourceforge.net>
2002-12-09 04:08:50 +0000

    added splitoff table

--- pdb/schema.sql
+++ pdb/schema.sql
@@ -10,10 +10,18 @@ create table sections (
  primary key (name)
 );
 
+create table splitoffs (
+ name varchar(64) not null,
+ parentkey varchar(84) not null,
+ descshort varchar(80) not null default '',
+ primary key(parentkey, name)
+};
+
 create table package (
  release varchar(20) not null,
  fullname varchar(128) not null,
  name varchar(64) not null,
+ parentname varchar(64),
  version varchar(64) not null,
  revision varchar (16) not null,
  section varchar(32) not null,
@@ -28,3 +36,4 @@ create table package (
  index (latest),
  index (needtest)
 );
+
