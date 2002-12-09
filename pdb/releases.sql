ec36772d5d5e27ec68111dbdff2eb43849bc3a8c
Ben Hines <benh57@users.sourceforge.net>
2002-12-09 04:06:42 +0000

    0.5.0a

--- pdb/releases.sql
+++ pdb/releases.sql
@@ -1,4 +1,5 @@
 delete from release;
 insert into release values ('current-stable', 'Packages that are in stable in CVS, updated daily.');
 insert into release values ('current-unstable', 'Packages that are in unstable in CVS, updated daily.');
-insert into release values ('0.4.1-stable', 'Packages shipped with Fink 0.4.1');
+insert into release values ('0.4.1-stable', 'Packages shipped with Fink 0.4.1 (For Mac OS X 10.1)');
+insert into release values ('0.5.0a-stable', 'Packages shipped with Fink 0.5.0a (For Mac OS X 10.2)');
