7ab162755bd8aaa4553c779e12ef7a8536e81f44
Ben Hines <benh57@users.sourceforge.net>
2003-01-18 01:07:40 +0000

    More automation! Also create /sw symlink for users

---
+++ installer/buildpkg.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+RESDIR=$IN_BASEDIR/resources$IN_VERSION;
+DMGDIR=$IN_BASEDIR/dmg$IN_VERSION;
+
+echo "basedir: $IN_BASEDIR version: $IN_VERSION";
+rm -rf $RESDIR
+rm -rf $DMGDIR
+chmod 1755 $IN_BASEDIR/contents
+cp -r $IN_BASEDIR/resources $RESDIR
+cp -r $IN_BASEDIR/dmg $DMGDIR
+perl -pi -e "s/IN_VERSION/$IN_VERSION/g" $RESDIR/ReadMe.rtf $RESDIR/Welcome.rtf $RESDIR/English.lproj/Description.plist $DMGDIR/Fink\ ReadMe.rtf
+echo "calling /Developer/Applications/PackageMaker.app/Contents/MacOS/PackageMaker -build -p \"$IN_BASEDIR/dmg/Fink $IN_VERSION Installer.pkg\" -f $IN_BASEDIR/contents -r $RESDIR -i $IN_BASEDIR/fink.info -d $RESDIR/English.lproj/Description.plist";
+
+/Developer/Applications/PackageMaker.app/Contents/MacOS/PackageMaker -build -p "$DMGDIR/Fink $IN_VERSION Installer.pkg" -f $IN_BASEDIR/contents -r $RESDIR -i $IN_BASEDIR/fink.info -d $RESDIR/English.lproj/Description.plist
--- installer/in_setup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sed -e "s+BASEDIR+$IN_BASEDIR+g" -e "s+VERSION+$IN_VERSION+g" < fink.pmsp.in > fink-$IN_VERSION.pmsp
