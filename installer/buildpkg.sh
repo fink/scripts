c838e771a33221d823f376a7c5e4b55087797244
Ben Hines <benh57@users.sourceforge.net>
2003-01-18 18:43:44 +0000

    make sure scripts are executable, and just use DMGDIR for CVS deletion

--- installer/buildpkg.sh
+++ installer/buildpkg.sh
@@ -6,10 +6,13 @@ echo "basedir: $IN_BASEDIR version: $IN_VERSION";
 rm -rf $RESDIR
 rm -rf $DMGDIR
 chmod 1755 $IN_BASEDIR/contents
+chmod a+x $IN_BASEDIR/resources/InstallationCheck
+chmod a+x $IN_BASEDIR/resources/postflight
+chmod a+x $IN_BASEDIR/resources/VolumeCheck
 cp -r $IN_BASEDIR/resources $RESDIR
 cp -r $IN_BASEDIR/dmg $DMGDIR
 perl -pi -e "s/IN_VERSION/$IN_VERSION/g" $RESDIR/ReadMe.rtf $RESDIR/Welcome.rtf $RESDIR/English.lproj/Description.plist $DMGDIR/Fink\ ReadMe.rtf
 echo "calling /Developer/Applications/PackageMaker.app/Contents/MacOS/PackageMaker -build -p \"$IN_BASEDIR/dmg/Fink $IN_VERSION Installer.pkg\" -f $IN_BASEDIR/contents -r $RESDIR -i $IN_BASEDIR/fink.info -d $RESDIR/English.lproj/Description.plist";
 
 /Developer/Applications/PackageMaker.app/Contents/MacOS/PackageMaker -build -p "$DMGDIR/Fink $IN_VERSION Installer.pkg" -f $IN_BASEDIR/contents -r $RESDIR -i $IN_BASEDIR/fink.info -d $RESDIR/English.lproj/Description.plist
-`find $DMGDIR/Fink\ $IN_VERSION\ Installer.pkg -name 'CVS' -type d -exec rm -rf {} \;`
+`find $DMGDIR -name 'CVS' -type d -exec rm -rf {} \;`
