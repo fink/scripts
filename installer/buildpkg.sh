c1e2ecf98110ae8732b70718dc3896e9cee5949a
Ben Hines <benh57@users.sourceforge.net>
2003-01-21 03:41:38 +0000

    chown it to root

--- installer/buildpkg.sh
+++ installer/buildpkg.sh
@@ -5,13 +5,14 @@ DMGDIR=$IN_BASEDIR/dmg-$IN_VERSION;
 echo "basedir: $IN_BASEDIR version: $IN_VERSION";
 rm -rf $RESDIR
 rm -rf $DMGDIR
+chown -R root:admin $IN_BASEDIR/contents 
 chmod 1755 $IN_BASEDIR/contents
 chmod a+x $IN_BASEDIR/resources/InstallationCheck
 chmod a+x $IN_BASEDIR/resources/postflight
 chmod a+x $IN_BASEDIR/resources/VolumeCheck
 cp -r $IN_BASEDIR/resources $RESDIR
 cp -r $IN_BASEDIR/dmg $DMGDIR
-perl -pi -e "s/IN_VERSION/$IN_VERSION/g" $RESDIR/ReadMe.rtf $RESDIR/Welcome.rtf $RESDIR/English.lproj/Description.plist $DMGDIR/Fink\ ReadMe.rtf
+perl -pi -e "s/IN_VERSwION/$IN_VERSION/g" $RESDIR/ReadMe.rtf $RESDIR/Welcome.rtf $RESDIR/English.lproj/Description.plist $DMGDIR/Fink\ ReadMe.rtf
 echo "calling /Developer/Applications/PackageMaker.app/Contents/MacOS/PackageMaker -build -p \"$IN_BASEDIR/dmg/Fink $IN_VERSION Installer.pkg\" -f $IN_BASEDIR/contents -r $RESDIR -i $IN_BASEDIR/fink.info -d $RESDIR/English.lproj/Description.plist";
 
 /Developer/Applications/PackageMaker.app/Contents/MacOS/PackageMaker -build -p "$DMGDIR/Fink $IN_VERSION Installer.pkg" -f $IN_BASEDIR/contents -r $RESDIR -i $IN_BASEDIR/fink.info -d $RESDIR/English.lproj/Description.plist
