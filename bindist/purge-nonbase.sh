cf341db4c3ce3f9538bc0dd58c239a2fc34d18c4
David R. Morrison <dmrrsn@users.sourceforge.net>
2003-10-11 21:47:46 +0000

    revise for newer essential packages

--- bindist/purge-nonbase.sh
+++ bindist/purge-nonbase.sh
@@ -6,25 +6,23 @@ dpkg --get-selections | \
 	grep -v '^apt-shlibs[[:space:]]' | \
 	grep -v '^base-files[[:space:]]' | \
 	grep -v '^bzip2[[:space:]]' | \
+	grep -v '^bzip2-shlibs[[:space:]]' | \
+	grep -v '^cctools-extra[[:space:]]' | \
 	grep -v '^debianutils[[:space:]]' | \
 	grep -v '^dpkg[[:space:]]' | \
 	grep -v '^fink[[:space:]]' | \
+	grep -v '^fink-prebinding[[:space:]]' | \
 	grep -v '^gettext[[:space:]]' | \
+	grep -v '^gettext-bin[[:space:]]' | \
 	grep -v '^gzip[[:space:]]' | \
 	grep -v '^libiconv[[:space:]]' | \
+	grep -v '^libiconv-bin[[:space:]]' | \
 	grep -v '^ncurses[[:space:]]' | \
+	grep -v '^ncurses-shlibs[[:space:]]' | \
 	grep -v '^storable-pm[[:space:]]' | \
+	grep -v '^storable-pm560[[:space:]]' | \
 	grep -v '^tar[[:space:]]' | \
-	grep -v '^xml-rss-pm[[:space:]]' | \
-	grep -v '^compress-zlib-pm[[:space:]]' | \
-	grep -v '^digest-md5-pm[[:space:]]' | \
-	grep -v '^html-parser-pm[[:space:]]' | \
-	grep -v '^html-tagset-pm[[:space:]]' | \
-	grep -v '^libnet-pm[[:space:]]' | \
-	grep -v '^libwww-pm[[:space:]]' | \
-	grep -v '^mime-base64-pm[[:space:]]' | \
-	grep -v '^uri-pm[[:space:]]' | \
-	grep -v '^xml-parser-pm[[:space:]]' | \
+	grep -v '^unzip[[:space:]]' | \
 	awk '{ print $1 }' | \
 	xargs sudo dpkg --purge
 
