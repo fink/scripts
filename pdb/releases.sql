d180719b933db928ad5ec3f86ac110bfa9bf3367
Ben Hines <benh57@users.sourceforge.net>
2003-11-03 01:47:41 +0000

    Updating PDB for new trees

--- pdb/releases.sql
+++ pdb/releases.sql
@@ -1,8 +1,12 @@
 delete from release;
-insert into release values ('current-stable', 'Packages that are in stable in CVS, updated daily.');
-insert into release values ('current-unstable', 'Packages that are in unstable in CVS, updated daily.');
+insert into release values ('current-10.3-stable', 'Packages that are in 10.3/stable in CVS, updated daily.');
+insert into release values ('current-10.3-unstable', 'Packages that are in 10.3/unstable in CVS, updated daily.');
+insert into release values ('current-10.2-stable', 'Packages that are in 10.2-gcc3.3/stable in CVS, updated daily.');
+insert into release values ('current-10.2-unstable', 'Packages that are in 10.2-gcc3.3/unstable in CVS, updated daily.');
+
 insert into release values ('0.4.1-stable', 'Packages shipped with Fink 0.4.1 (For Mac OS X 10.1)');
 insert into release values ('0.5.0a-stable', 'Packages shipped with Fink 0.5.0a (For Mac OS X 10.2)');
 insert into release values ('0.5.1-stable', 'Packages shipped with Fink 0.5.1 (For Mac OS X 10.2)');
 insert into release values ('0.5.2-stable', 'Packages shipped with Fink 0.5.2 (For Mac OS X 10.2)');
 insert into release values ('0.5.3-stable', 'Packages shipped with Fink 0.5.3 (For Mac OS X 10.2)');
+insert into release values ('0.6.1-stable', 'Packages shipped with Fink 0.6.1 (For Mac OS X 10.2/10.3)');
