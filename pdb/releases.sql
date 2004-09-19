db2199354ec2029e3000581cab47d8c15d6fd607
Ben Hines <benh57@users.sourceforge.net>
2004-09-19 23:23:31 +0000

    Add 0.7.1

--- pdb/releases.sql
+++ pdb/releases.sql
@@ -13,3 +13,4 @@ insert into release values ('0.6.1-stable', 'Packages shipped with Fink 0.6.1 (F
 insert into release values ('0.6.2-stable', 'Packages shipped with Fink 0.6.2 (For Mac OS X 10.2/10.3)');
 insert into release values ('0.6.3-stable', 'Packages shipped with Fink 0.6.3 (For Mac OS X 10.2)');
 insert into release values ('0.7.0-stable', 'Packages shipped with Fink 0.7.0 (For Mac OS X 10.3)');
+insert into release values ('0.7.1-stable', 'Packages shipped with Fink 0.7.0 (For Mac OS X 10.3)');
