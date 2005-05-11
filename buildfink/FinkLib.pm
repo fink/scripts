2e3aaf316ebcb1e2b72572fa99449c6a54bb6ef7
Matthew Sachs <msachs@users.sourceforge.net>
2005-05-11 19:04:33 +0000

    Initial commit

---
+++ buildfink/FinkLib.pm
@@ -0,0 +1,47 @@
+# A library of functions for interacting with Fink
+
+#Copyright (c) 2005 Apple Computer, Inc.  All Rights Reserved.
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+package FinkLib;
+use strict;
+use warnings;
+
+our($FinkDir, $FinkConfig);
+
+# Initialize the Fink API
+sub initFink($) {
+	$FinkDir = shift;
+
+	unshift @INC, $FinkDir . "/lib/perl5";
+	require Fink::Config;
+	require Fink::Services;
+	require Fink::Package;
+	require Fink::PkgVersion;
+	require Fink::VirtPackage;
+	require Fink::Status;
+
+	$ENV{PATH} = "$FinkDir/bin:$FinkDir/sbin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/X11R6/bin";
+	$ENV{PERL5LIB} = "$FinkDir/lib/perl5:$FinkDir/lib/perl5/darwin";
+
+	$FinkConfig = Fink::Services::read_config("$FinkDir/etc/fink.conf");
+	Fink::Config::set_options({Verbose => 3, KeepBuildDir => 1});
+	Fink::Package->require_packages();
+
+	return $FinkConfig;
+}
+
+1;
