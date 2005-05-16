7420aec9e1815bd3b2801fbcf4c1b6e0a69a3801
Daniel Macks <dmacks@users.sourceforge.net>
2005-05-16 20:44:05 +0000

    Update to match current PDB contents.

--- pdb/releases.sql
+++ pdb/releases.sql
@@ -1,8 +1,8 @@
 delete from release;
 insert into release values ('current-10.3-stable', 'Packages that are in 10.3/stable in CVS, updated daily.');
 insert into release values ('current-10.3-unstable', 'Packages that are in 10.3/unstable in CVS, updated daily.');
-insert into release values ('current-10.2-gcc3.3-stable', 'Packages that are in 10.2-gcc3.3/stable in CVS, updated daily.');
-insert into release values ('current-10.2-gcc3.3-unstable', 'Packages that are in 10.2-gcc3.3/unstable in CVS, updated daily.');
+insert into release values ('current-10.4-stable', 'Packages that are in 10.4/stable in CVS, updated daily.');
+insert into release values ('current-10.4-unstable', 'Packages that are in 10.4/unstable in CVS, updated daily.');
 
 insert into release values ('0.4.1-stable', 'Packages shipped with Fink 0.4.1 (For Mac OS X 10.1)');
 insert into release values ('0.5.0a-stable', 'Packages shipped with Fink 0.5.0a (For Mac OS X 10.2)');
