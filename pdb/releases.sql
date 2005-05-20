e744e81ba6ac0baa1fe4d77f090f75e9f36046a0
Daniel Macks <dmacks@users.sourceforge.net>
2005-05-20 17:02:16 +0000

    Okay drm, we'll not EOL 10.2-gcc3.3 source-dist yet.

--- pdb/releases.sql
+++ pdb/releases.sql
@@ -1,4 +1,7 @@
 delete from release;
+
+insert into release values ('current-10.2-gcc3.3-stable', 'Packages that are in 10.2-gcc3.3/stable in CVS, updated daily.');
+insert into release values ('current-10.2-gcc3.3-unstable', 'Packages that are in 10.2-gcc3.3/unstable in CVS, updated daily.');
 insert into release values ('current-10.3-stable', 'Packages that are in 10.3/stable in CVS, updated daily.');
 insert into release values ('current-10.3-unstable', 'Packages that are in 10.3/unstable in CVS, updated daily.');
 insert into release values ('current-10.4-stable', 'Packages that are in 10.4/stable in CVS, updated daily.');
