123841f808c6881b93cba4b2baa72178166d36f4
Daniel Macks <dmacks@users.sourceforge.net>
2006-04-04 21:43:23 +0000

    Use fink's own setup script instead of trying to fake it with sed

--- pdb/update.sh
+++ pdb/update.sh
@@ -4,12 +4,14 @@ echo "Updating CVS..."
 cvs -q update -dP
 cd basepath
 cvs -q update -dP
-cd perlmod/Fink
-rm -f FinkVersion.pm
-sed "s|@VERSION@|`cat ../../VERSION`|" < FinkVersion.pm.in > FinkVersion.pm
-cd ../../..
+cd ..
 
 echo "Fixing permissions"
 chgrp -R fink .
 chmod -f -R g+w,a+r .
 chmod o-rwx db.inc.pl
+
+echo "Creating fink itself"
+cd basepath
+./setup.sh $PWD/basepath
+cd ..
