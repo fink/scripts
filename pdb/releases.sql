14b26c8cabddcff7c8ade25202bdbcb7792327bf
Daniel Macks <dmacks@users.sourceforge.net>
2006-06-22 01:52:36 +0000

    synchronize with reality

--- pdb/releases.sql
+++ pdb/releases.sql
@@ -6,13 +6,13 @@ insert into release values ('current-10.3-stable', 'Packages that are in 10.3/st
 insert into release values ('current-10.3-unstable', 'Packages that are in 10.3/unstable in CVS, updated daily.');
 insert into release values ('current-10.4-transitional-stable', 'Packages that are in 10.4-transitional/stable in CVS, updated daily.');
 insert into release values ('current-10.4-transitional-unstable', 'Packages that are in 10.4-transitional/unstable in CVS, updated daily.');
-insert into release values ('current-10.4-stable-powerpc', 'Packages that are in 10.4/stable in CVS for arch=powerpc, updated daily.');
-insert into release values ('current-10.4-unstable-powerpc', 'Packages that are in 10.4/unstable in CVS for arch=powerpc, updated daily.');
-insert into release values ('current-10.4-stable-i386', 'Packages that are in 10.4/stable in CVS for arch=i386, updated daily.');
-insert into release values ('current-10.4-unstable-386', 'Packages that are in 10.4/unstable in CVS for arch=i386, updated daily.');
+insert into release values ('current-10.4-powerpc-stable', 'Packages that are in 10.4/stable in CVS for arch=powerpc, updated daily.');
+insert into release values ('current-10.4-powerpc-unstable-powerpc', 'Packages that are in 10.4/unstable in CVS for arch=powerpc, updated daily.');
+insert into release values ('current-10.4-i386-stable', 'Packages that are in 10.4/stable in CVS for arch=i386, updated daily.');
+insert into release values ('current-10.4-i386-unstable', 'Packages that are in 10.4/unstable in CVS for arch=i386, updated daily.');
 
 insert into release values ('0.4.1-stable', 'Packages shipped with Fink 0.4.1 (For Mac OS X 10.1)');
-insert into release values ('0.5.3-stable', 'Packages shipped with Fink 0.5.3 (For Mac OS X 10.2)');
+insert into release values ('0.5.2-stable', 'Packages shipped with Fink 0.5.3 (For Mac OS X 10.2)');
 insert into release values ('0.6.3-stable', 'Packages shipped with Fink 0.6.3 (For Mac OS X 10.2)');
 insert into release values ('0.6.4-stable', 'Packages shipped with Fink 0.6.4 (For Mac OS X 10.2)');
 insert into release values ('0.7.1-stable', 'Packages shipped with Fink 0.7.1 (For Mac OS X 10.3)');
